e7b09aa936795b51d2106c5f0900606805939ef8
Kristen Bair <kristenmbair@gmail.com>
2021-03-15 17:20:20 -0600

    Add new action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,21 @@
 export const addToDo = todo => ({
   type: 'ADD_TODO',
   todo
-});
+})
 
 export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
+})
+
+export const showAll = () => ({
+  type: 'SHOW_ALL'
+})
+
+export const showActive = () => ({
+  type: 'SHOW_ACTIVE'
+})
+
+export const showCompleted = () => ({
+  type: 'SHOW_COMPLETED'
 })
