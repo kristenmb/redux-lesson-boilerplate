2788ecb49f9a734ca1700027cc2d0d8ae97afd01
Kristen Bair <kristenmbair@gmail.com>
2021-03-15 18:28:30 -0600

    Create conditional display of todos based on which button is clicked

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -3,17 +3,43 @@ import { connect } from 'react-redux';
 import ToDoCard from '../components/ToDoCard'
 import { showAll, showActive, showCompleted} from '../actions/index'
 
-const ToDoList = ({todos, showAll, showActive, showCompleted}) => {
-  const todoDisplay = todos.map(todo => {
-    return (
-      <ToDoCard
-        key={todo.id}
-        id={todo.id}
-        todo={todo.text}
-        completed={todo.completed}
-      />
-    )
-  })
+const ToDoList = ({todos, completed, active, clicked, showAll, showActive, showCompleted}) => {
+  const todoDisplay = () => {
+    if (clicked === 'active') {
+      return active.map(todo => {
+        return (
+          <ToDoCard
+            key={todo.id}
+            id={todo.id}
+            todo={todo.text}
+            completed={todo.completed}
+          />
+        )
+    })
+    } else if (clicked === 'completed') {
+      return completed.map(todo => {
+        return (
+          <ToDoCard
+            key={todo.id}
+            id={todo.id}
+            todo={todo.text}
+            completed={todo.completed}
+          />
+         )
+    })
+    } else if (clicked === 'all') {
+      return todos.map(todo => {
+        return (
+          <ToDoCard
+            key={todo.id}
+            id={todo.id}
+            todo={todo.text}
+            completed={todo.completed}
+          />
+        )
+    })
+    }
+}
 
   return (
      <section>
@@ -21,14 +47,17 @@ const ToDoList = ({todos, showAll, showActive, showCompleted}) => {
       <button onClick={() => showActive()}>Active</button>
       <button onClick={() => showCompleted()}>Done</button>
       <ul>
-        {todoDisplay}
+        {todoDisplay()}
       </ul>
     </section>
   )
 }
 
 const mapStateToProps = state => ({
-  todos: state.todos
+  todos: state.todos.todos,
+  completed: state.todos.completed,
+  active: state.todos.active,
+  clicked: state.todos.clicked
 });
 
 const mapDispatchToProps = dispatch => ({
